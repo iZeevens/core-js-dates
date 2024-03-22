b035d53b39ff66ae3f3a7f2088b86b21aa36aab4
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-03-22 09:06:14 +0000

    refactor: getNextFriday

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -66,8 +66,16 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const newDate = new Date(date);
+  const weekday = newDate.getDay();
+  const indexFriday = 5;
+  let daysUntilFriday = indexFriday - weekday;
+  if (daysUntilFriday <= 0) {
+    daysUntilFriday += 7;
+  }
+  newDate.setDate(newDate.getDate() + daysUntilFriday);
+  return newDate;
 }
 
 /**
